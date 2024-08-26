fe2199570f37a699c6677a95ebdf8313b9820342
Hatvani Sándor <hatvani.sandor@gmail.com>
2024-08-26 19:58:58 +0200

    Adding Fake Authentication: Protecting a Route

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ import CountryList from "./components/CountryList";
 import Form from "./components/Form";
 import { CitiesProvider } from "./contexts/CitiesContext";
 import { AuthProvider } from "./contexts/FakeAuthContext";
+import ProtectedRoutr from "./pages/ProtectedRoutr";
 
 polyfillCountryFlagEmojis();
 
@@ -26,7 +27,14 @@ function App() {
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
             <Route path="login" element={<Login />} />
-            <Route path="app" element={<AppLayout />}>
+            <Route
+              path="app"
+              element={
+                <ProtectedRoutr>
+                  <AppLayout />
+                </ProtectedRoutr>
+              }
+            >
               <Route index element={<Navigate replace to="cities" />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
